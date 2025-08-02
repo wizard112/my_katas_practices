e366f235123414e1c786230090031763039ac753
Your Name <gael@MacBook-Pro-M1-de-Gael-Tshilombo.local>
2025-08-02 12:41:35 +0200

    refactor(): add column as parameter

--- app/src/main/java/com/rossel/android/sdk/mykatas/domain/entity/TicTacToe.kt
+++ app/src/main/java/com/rossel/android/sdk/mykatas/domain/entity/TicTacToe.kt
@@ -1,6 +1,29 @@
 package com.rossel.android.sdk.mykatas.domain.entity
 
-class TicTacToe {
+class Column {
+    var player: String = ""
+    var position: Int = -1
+}
+class Row {
+    var player: String = ""
+    var position: Int = -1
+}
+data class Player(var name: String = "")
 
-    fun isGameOver(): Boolean = true
+class TicTacToe {
+    //private var columns: MutableList<Column> = mutableListOf()
+    //private var rows: MutableList<Row> = mutableListOf()
+    //private var players: MutableList<Player> = mutableListOf()
+    //private var player: Player
+
+    /*init {
+        players.add(element = Player(name = "X"))
+        players.add(element = Player(name = "O"))
+    }*/
+
+    fun isGameOver(column: Int): Boolean = true
+
+    /*fun takesTurn() {
+
+    }*/
 }
--- app/src/test/java/com/rossel/android/sdk/mykatas/TicTacToeTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mykatas/TicTacToeTest.kt
@@ -10,6 +10,21 @@ class TicTacToeTest {
 
     @Test
     fun `should the game is over when all fields in a row are taken by a player`() {
-        Assert.assertEquals(true, ticTacToe.isGameOver())
+        Assert.assertEquals(true, ticTacToe.isGameOver(column = 1))
+    }
+
+    @Test
+    fun `should the game is over when all fields in a column are taken by a player`() {
+        Assert.assertEquals(true, ticTacToe.isGameOver(column = 1))
+    }
+
+    @Test
+    fun `should the game is over when all fields in a diagonal are taken by a player`() {
+        Assert.assertEquals(true, ticTacToe.isGameOver(column = 1))
+    }
+
+    @Test
+    fun `should the game is over when all fields are taken`() {
+        Assert.assertEquals(true, ticTacToe.isGameOver(column = 1))
     }
 }
