c9034862d31b6698e68e810cd4aa864467dc9bea
Your Name <gael@MacBook-Pro-M1-de-Gael-Tshilombo.local>
2025-08-02 13:49:28 +0200

    refactor(): check if columns or rows are taken by a player

--- app/src/main/java/com/rossel/android/sdk/mykatas/domain/entity/TicTacToe.kt
+++ app/src/main/java/com/rossel/android/sdk/mykatas/domain/entity/TicTacToe.kt
@@ -1,13 +1,14 @@
 package com.rossel.android.sdk.mykatas.domain.entity
 
-class Column {
+/*class Column {
     var player: String = ""
     var position: Int = -1
 }
 class Row {
     var player: String = ""
     var position: Int = -1
-}
+}*/
+
 data class Player(var name: String = "")
 
 class TicTacToe {
@@ -21,14 +22,19 @@ class TicTacToe {
         players.add(element = Player(name = "O"))
     }*/
 
-    private var columns = hashMapOf<Int, Boolean>()
-    private var rows = hashMapOf<Int, Boolean>()
+    private var columns = hashMapOf<Int, Player>()
+    private var rows = hashMapOf<Int, Player>()
     fun isGameOver(): Boolean {
-        return (columns.values.filter { it }.size == 3 || rows.values.filter { it }.size == 3)
+        return (columns[1]?.name == "X" &&
+                (rows[1]?.name == "X" || rows[2]?.name == "X" || rows[3]?.name == "X" )
+                || columns[2]?.name == "X" &&
+                (rows[1]?.name == "X" || rows[2]?.name == "X" || rows[3]?.name == "X" )
+                || columns[3]?.name == "X" &&
+                (rows[1]?.name == "X" || rows[2]?.name == "X" || rows[3]?.name == "X" ))
     }
-    fun play(column: Int, row: Int, player: String) {
-        columns[column] = true
-        rows[row] = true
+    fun play(column: Int, row: Int, player: Player) {
+        columns[column] = player
+        rows[row] = player
     }
 
     /*fun takesTurn() {
--- app/src/test/java/com/rossel/android/sdk/mykatas/TicTacToeTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mykatas/TicTacToeTest.kt
@@ -1,5 +1,6 @@
 package com.rossel.android.sdk.mykatas
 
+import com.rossel.android.sdk.mykatas.domain.entity.Player
 import com.rossel.android.sdk.mykatas.domain.entity.TicTacToe
 import org.junit.Assert
 import org.junit.Test
@@ -10,33 +11,39 @@ class TicTacToeTest {
 
     @Test
     fun `should the game is over when all fields in a row are taken by a player`() {
-        ticTacToe.play(column = 1, row = 1, player = "X")
-        ticTacToe.play(column = 1, row = 2, player = "O")
-        ticTacToe.play(column = 1, row = 3, player = "X")
+        ticTacToe.play(column = 1, row = 1, player = Player(name = "X"))
+        ticTacToe.play(column =  2, row = 1, player = Player(name = "X"))
+        ticTacToe.play(column =  3, row = 1, player = Player(name = "X"))
         Assert.assertEquals(true, ticTacToe.isGameOver())
     }
 
     @Test
     fun `should the game is over when all fields in a column are taken by a player`() {
-        ticTacToe.play(column = 1, row = 1, player = "X")
-        ticTacToe.play(column = 2, row = 1, player = "X")
-        ticTacToe.play(column = 3, row = 1, player = "X")
+        ticTacToe.play(column =  1, row = 1, player = Player(name = "X"))
+        ticTacToe.play(column =  1, row = 2, player = Player(name = "X"))
+        ticTacToe.play(column =  1, row = 3, player = Player(name = "X"))
         Assert.assertEquals(true, ticTacToe.isGameOver())
     }
 
     @Test
     fun `should the game is over when all fields in a diagonal are taken by a player`() {
-        ticTacToe.play(column = 1, row = 1, player = "X")
-        ticTacToe.play(column = 5, row = 1, player = "O")
-        ticTacToe.play(column = 9, row = 1, player = "O")
+        ticTacToe.play(column = 1, row = 1, player = Player(name = "X"))
+        ticTacToe.play(column = 5, row = 1, player = Player(name = "O"))
+        ticTacToe.play(column = 9, row = 1, player = Player(name = "O"))
         Assert.assertEquals(true, ticTacToe.isGameOver())
     }
 
     @Test
     fun `should the game is over when all fields are taken`() {
-        ticTacToe.play(column = 1, row = 1, player = "O")
-        ticTacToe.play(column = 2, row = 1, player = "0")
-        ticTacToe.play(column = 3, row = 1, player = "X")
+        ticTacToe.play(column = 1, row = 1, player = Player(name = "O"))
+        ticTacToe.play(column = 1, row = 2, player = Player(name = "X"))
+        ticTacToe.play(column = 1, row = 3, player = Player(name = "0"))
+        ticTacToe.play(column = 2, row = 1, player = Player(name = "X"))
+        ticTacToe.play(column = 2, row = 2, player = Player(name = "0"))
+        ticTacToe.play(column = 2, row = 3, player = Player(name = "X"))
+        ticTacToe.play(column = 3, row = 1, player = Player(name = "X"))
+        ticTacToe.play(column = 3, row = 2, player = Player(name = "0"))
+        ticTacToe.play(column = 3, row = 3, player = Player(name = "X"))
         Assert.assertEquals(true, ticTacToe.isGameOver())
     }
 }
