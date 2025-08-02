6f60f2340a5dc89a33405f6c37c9572a347d907c
Your Name <gael@MacBook-Pro-M1-de-Gael-Tshilombo.local>
2025-08-02 02:48:50 +0200

    test(): implement - prints fizz instead of the number if the number is a multiple of three

--- app/src/test/java/com/rossel/android/sdk/mykatas/FizzBuzzTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mykatas/FizzBuzzTest.kt
@@ -1,11 +1,14 @@
 package com.rossel.android.sdk.mykatas
 
+import com.rossel.android.sdk.mykatas.data.extensions.fizzBuzz
 import org.junit.Assert
 import org.junit.Test
 
 class FizzBuzzTest {
     @Test
     fun `prints fizz instead of the number if the number is a multiple of three`() {
-        Assert.assertEquals("Fizz", fizzbuzz())
+        Assert.assertEquals("Fizz", 3.fizzBuzz())
+        Assert.assertEquals("Fizz", 6.fizzBuzz())
+        Assert.assertEquals("Fizz", 9.fizzBuzz())
     }
 }
