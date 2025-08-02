51828a2e5284187e31ccc2bef16c8a0727ae5205
Your Name <gael@MacBook-Pro-M1-de-Gael-Tshilombo.local>
2025-08-02 11:58:15 +0200

    refactor(): improve name of test function

--- app/src/test/java/com/rossel/android/sdk/mykatas/FizzBuzzTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mykatas/FizzBuzzTest.kt
@@ -6,20 +6,20 @@ import org.junit.Test
 
 class FizzBuzzTest {
     @Test
-    fun `prints Fizz instead of the number if the number is a multiple of three`() {
+    fun `should prints Fizz when number is a multiple of 3`() {
         Assert.assertEquals("Fizz", 3.fizzBuzz())
         Assert.assertEquals("Fizz", 6.fizzBuzz())
         Assert.assertEquals("Fizz", 9.fizzBuzz())
     }
 
     @Test
-    fun `prints Buzz instead of the number if the number is a multiple of five`() {
+    fun `should prints Buzz when number is a multiple of 5`() {
         Assert.assertEquals("Buzz", 5.fizzBuzz())
         Assert.assertEquals("Buzz", 10.fizzBuzz())
     }
 
     @Test
-    fun `prints FizzBuzz instead of the number if the number is a multiple of both three and five`() {
+    fun `should prints FizzBuzz when number is a multiple of both 3 and 5`() {
         Assert.assertEquals("FizzBuzz", 15.fizzBuzz())
         Assert.assertEquals("FizzBuzz", 30.fizzBuzz())
         Assert.assertEquals("FizzBuzz", 45.fizzBuzz())
