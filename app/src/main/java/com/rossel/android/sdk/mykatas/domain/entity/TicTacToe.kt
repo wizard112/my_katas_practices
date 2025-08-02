e56e50f18cacb88552d41c8b0b0ae20705cb8264
Your Name <gael@MacBook-Pro-M1-de-Gael-Tshilombo.local>
2025-08-02 12:28:04 +0200

    feat(): implement if the game is over

---
+++ app/src/main/java/com/rossel/android/sdk/mykatas/domain/entity/TicTacToe.kt
@@ -0,0 +1,6 @@
+package com.rossel.android.sdk.mykatas.domain.entity
+
+class TicTacToe {
+
+    fun isGameOver(): Boolean = true
+}
--- app/src/test/java/com/rossel/android/sdk/mykatas/TicTacToeTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mykatas/TicTacToeTest.kt
@@ -1,12 +1,15 @@
 package com.rossel.android.sdk.mykatas
 
+import com.rossel.android.sdk.mykatas.domain.entity.TicTacToe
 import org.junit.Assert
 import org.junit.Test
 
 class TicTacToeTest {
 
+    private val ticTacToe = TicTacToe()
+
     @Test
     fun `should the game is over when all fields in a row are taken by a player`() {
-        Assert.assertEquals(true, isGameOver())
+        Assert.assertEquals(true, ticTacToe.isGameOver())
     }
 }
